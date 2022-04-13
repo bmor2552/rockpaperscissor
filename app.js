fa8dce8b9ed50fa31704043c59a343be49659837
bmor2552 <boi.moriba@gmail.com>
2022-04-13 18:54:19 -0500

    Working on scripts

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-/*computerPlay(playerSelection , computerSelection){
-return "You Lose! Paper beats Rock";
-}*/
+const choices = ["rock", "paper", "scissors"]
+
+function game (){
+    playRound(){
+
+    }
+}
+
+function playRound(computerSelection){
+const playerSelection = 
+}
+
+
+function playerChoice(){
+
+}
+
+function computerChoice(){
+    return choices[Math.floor(Math.random() * choices.length)]
+}
