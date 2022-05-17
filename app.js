8fb90ae4d81b18ec1c2be6f5eb39b794d6ed8a40
bmor2552 <boi.moriba@gmail.com>
2022-05-17 06:54:36 -0500

    Problem Break Down

--- app.js
+++ app.js
@@ -1,21 +1,62 @@
+
 const choices = ["rock", "paper", "scissors"];
 
-function game (){
-    playRound(){
+/* Problem Break Down 
+Choices 
+-----------------
+-----------------
+Rock Paper Scissors 
 
-    }
-}
+Rules 
+-----------------
+-----------------
+5 Rounds of RPS
+Rock > Scissors
+Scissors > Paper 
+Paper > Rock
 
-function playRound(computerSelection){
-    const playerSelection = playerChoice();
-    const computerSelection = computerChoice();
-}
+Statements
+-----------------
+-----------------
+If Neo wins Zion is saved 
+If The Architect wins he gets to reset the matrix and destroy Zion 
+
+Winning Statement 
+Zion has been saved! 
+Play again will appear after 
+
+Losing Statement 
+Rest assure this will be the six time we have destroyed it and we have become exceedingly efficient at it.
+Try again will appear after 
 
 
-function playerChoice(){
-    let input = prompt("Type")
-}
+Game Resets (not on loading page but at the page where you can play the game again)
 
-function computerChoice(){
-    return choices[Math.floor(Math.random() * choices.length)];
-}
+Players 
+-----------------
+-----------------
+P1: Neo 
+P2: The Architect 
+
+
+*/
+
+
+/* Pseudo Code 
+
+create choices = [rock, paper, scissors]
+
+create player 1 Neo
+create player 2 The Architect 
+
+function for RPS Game (Neo, The Architect) takes the two players as parameters for the game 
+    function to tally score ()
+    function to reset after game ()
+
+function for Neo ()
+function for The Architect ()
+
+
+
+
+ */
